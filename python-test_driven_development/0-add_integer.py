5203fffeac6a91c9aa8394707030a94adfd8d573
RohMeOh <romeogr21@gmail.com>
2026-05-17 00:53:11 -0400

    Fix add_integer docstrings

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-"""This module contains a function that adds two integers."""
+"""This module defines an integer addition function.
+
+The function accepts integers and floats.
+Floats are converted to integers before addition.
+"""
 
 
 def add_integer(a, b=98):
-    """Add two integers.
+    """Return the addition of two integers.
 
-    Args:
-        a: first integer or float
-        b: second integer or float
-
-    Returns:
-        The addition of a and b as an integer.
-
-    Raises:
-        TypeError: if a or b is not an integer or float
+    Raises TypeError if a or b is not an integer or float.
     """
-
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
 
