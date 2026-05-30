aa661c7e029d15ab2231d77e53f18eee9ffe3e84
RohMeOh <romeogr21@gmail.com>
2026-05-30 06:34:54 -0400

    Task 10: Square #1

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Module containing Square class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size):
+        """Initialize square"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
